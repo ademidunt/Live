4c4c70ef7e17587f0400524e2a0500397238d143
wsucklal <warrensucklal@gmail.com>
2023-11-16 17:25:16 -0500

    created profile header and handler

--- frontend/Live/src/screens/ProfileScreen/ProfileScreen.js
+++ frontend/Live/src/screens/ProfileScreen/ProfileScreen.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
+const ProfileHandler = require('../../handlers/ProfileHandler')
 const styles = require('./ProfileScreenStyles')
 
+import ProfileHeader from "../../components/ProfileHeader/ProfileHeader";
+
+const text = ProfileHandler.getMovies();
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Profile Screen</Text>
+      <ProfileHeader/>
+      <ScrollView style={styles.scrollview}>
+        <Text style={styles.text}>Name</Text>
+        <View style={styles.emailContainer}>
+          <Text style={styles.text}>Email:</Text>
+          <Text style={styles.text}>Email:</Text>
+        </View>
+        <Text style={styles.text}>Profile Screen</Text>
+        <Text style={styles.text}>Email:</Text>
+        <Text style={styles.text}>Test Screen</Text>
+        <Text style={styles.text}>Phone Number:</Text>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
--- frontend/Live/src/screens/ProfileScreen/ProfileScreenStyles.js
+++ frontend/Live/src/screens/ProfileScreen/ProfileScreenStyles.js
@@ -6,12 +6,25 @@ const styles = StyleSheet.create({
       backgroundColor: '#1E1E1E',
       alignItems: 'center',
       justifyContent: 'center',
-      borderTopColor: '#4709CD'
+      //borderTopColor: '#red'
     },
   
     text:{
       color: '#FFFFFF',
-    }
+    },
+
+    emailContainer:{
+      flex: 1,
+      flexDirection: 'row',
+      columnGap: 10
+    },
+
+    scrollview:{
+      flex: 0.5,
+      backgroundColor:'red',
+      padding: 20
+    },
+
   });
 
 module.exports = styles ;
