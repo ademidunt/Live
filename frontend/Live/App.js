4c1e0c7eaa4de6a0e4257dba6076bc616a333573
wsucklal <warrensucklal@gmail.com>
2023-11-16 17:23:29 -0500

    added in app Styles

--- frontend/Live/App.js
+++ frontend/Live/App.js
@@ -1,38 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Tab = createBottomTabNavigator();
-
 import ProfileScreen from "./src/screens/ProfileScreen/ProfileScreen";
 import SearchScreen from "./src/screens/SearchScreen/SearchScreen";
+
+const Tab = createBottomTabNavigator();
+
+const tabNavigator = require('./AppStyles')
 
 export default function App() {
   return(
   <NavigationContainer>
     <Tab.Navigator 
-    initialRouteName='./src/screens/SearchScreen/SearchScreen'
-    screenOptions={{
-      headerShown: false,
-      headerTintColor: '#FFFFFF',
-      headerStyle: {
-        backgroundColor: "#4709CD",
-        borderBottomColor: "#4709CD"
-      },
-
-      tabBarActiveTintColor: '#FFFFFF',
-      tabBarInactiveTintColor: "#9166ED",
-
-      tabBarStyle: {
-        backgroundColor: '#4709CD',
-        borderTopColor: "#4709CD"
-      } 
-    }}
+    initialRouteName={tabNavigator.initialRouteName}
+    screenOptions={tabNavigator.screenOptions}
     >
       <Tab.Screen name="Search" component={SearchScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} options={{headerShown: true}}/>
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   </NavigationContainer>
   );
