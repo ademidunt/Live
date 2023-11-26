44c63fcdf61f518fb16fd70d73089c6060f0ad59
jtan334 <jtan334@uwo.ca>
2023-11-25 19:32:51 -0500

    Added imports for auth

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { getUsers , getUser, createUser, updateUser } from '../controllers/userOps.js';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../firebase/firebase.js';
 
 const app = express();
 const port = 3000;
