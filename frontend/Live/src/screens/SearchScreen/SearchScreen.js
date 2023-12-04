476ed9c432a0265030bf7b13431af69106a5a9fd
bdavidov8771 <bdavidov@uwo.ca>
2023-12-03 20:34:59 -0500

    Add event post component

--- frontend/Live/src/screens/SearchScreen/SearchScreen.js
+++ frontend/Live/src/screens/SearchScreen/SearchScreen.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import EventPost from '../../components/EventPost';
 
 const styles = require('./SearchScreenStyles')
 
 export default function SearchScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+      {/* <EventPost/> */}
       <StatusBar style="auto" />
     </View>
   );
