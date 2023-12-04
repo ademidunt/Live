bd0b066b19d2775fcbb5daf05e4d41d578fce920
ademidunt <71097538+ademidunt@users.noreply.github.com>
2023-12-04 11:20:05 -0500

    last working iteration

--- backend/controllers/clubberOps.js
+++ backend/controllers/clubberOps.js
@@ -75,37 +75,37 @@ export async function createClubber(clubber) {
 }
 
 export async function uploadImage(imageData) {
-    return new Promise((resolve, reject) => {
-        console.log('upload Image function in clubber ops is hit');
+
+        const metadata = {
+            'Content-type': 'image/jpeg',
+        }
+   
+        console.log('the data being sent is ', imageData );
         const storageRef = ref(storage, "images/" + imageData.name);
-        const uploadTask = uploadBytesResumable(storageRef, imageData.blob);
+        const uploadTask = uploadBytesResumable(storageRef, imageData.blob, metadata );
 
         // listen for events
         uploadTask.on(
             "state_changed",
-            // (snapshot) => {
-            //     const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            //     console.log("Upload is ", progress + "% done");
-            // },
+            (snapshot) => {
+                //bytes are not being transfered 
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log("Upload is ", progress + "% done");
+            },
             (error) => {
                 console.error("Error during upload:", error);
                 reject(error); // handle error
             },
             async () => {
-                try {
+
                     const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
                     console.log("File available at ", downloadURL);
 
-                    resolve(downloadURL); // resolve the promise with the download URL
+                    return downloadURL; // resolve the promise with the download URL
 
-                } catch (error) {
-                    console.error("Error getting download URL:", error);
-                    reject(error); // handle error in getting download URL
-                }
-            }
-        );
-    });
+            })
 }
+
 
 
 //Update clubber.
