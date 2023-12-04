1e26fed7b20dbef81bafa166acae82f57716a1ce
tprabhakaran <tprabhak@uwo.ca>
2023-12-03 20:05:13 -0500

    add clubber sign in

---
+++ frontend/Live/src/screens/SignInScreen/ClubSignInScreen.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+
+const SignInScreen = () => {
+  // State variables to manage email and password input fields
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Function to handle the login process
+  const handleSignIn = () => {
+    // Validation: Check if email and password are not empty
+    if (!email || !password) {
+      // Alert the user if fields are incomplete
+      Alert.alert('Incomplete Information', 'Please enter email and password.');
+      return;
+
+    
+      const userData = {
+        email,
+        password
+      };
+
+      const updateDatabase = async () => {
+        console.log(`new session`);
+        fetch(`http://192.168.2.50:3000/venue/login`, {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify(userData),
+        })
+          .then(async (res) => {
+            if (res.ok) {
+              console.log(`added to the database successfully`);
+            } else {
+              console.log(`something went wrong ${JSON.stringify(res)}`);
+            }
+          });
+      };
+    }
+    updateDatabase();
+
+
+
+    // Implement logic for authenticating user (communicate with backend, etc.)
+    
+
+    // For demonstration purposes, simply logging the entered credentials
+    console.log('Login Attempt:', { email, password });
+
+    // Clear email and password fields after login attempt (for demonstration)
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* Email input field */}
+      <Text>Email:</Text>
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        keyboardType="email-address" // Set keyboard type to email
+        autoCapitalize="none" // Disable auto-capitalization for email
+      />
+
+      {/* Password input field */}
+      <Text>Password:</Text>
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry={true} // Mask text input for password
+      />
+
+      {/* Button to trigger the login process */}
+      <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
+
+export default SignInScreen;
