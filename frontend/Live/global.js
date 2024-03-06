5a448a3009a3a020bf2d13805c8d6494bcaf3905
Eric Klassen <30785238+eklass3@users.noreply.github.com>
2024-03-06 18:02:24 +0100

    Now using location ordered venues for search page.

---
+++ frontend/Live/global.js
@@ -0,0 +1,11 @@
+// // //school url
+global.apiUrl = 'http://192.168.0.116:3000'
+
+//apartment url
+// global.apiUrl ='http://192.168.1.141:3000'
+
+// //phone hotshpo 
+// global.apiUrl ='http://172.20.10.2:3000'
+
+//vienna url
+//global.apiUrl = 'http://192.168.253.50:3000'
