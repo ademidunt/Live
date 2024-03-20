c429a4bb488d1f0f11c068b39fe95bb3514e04a2
jtan334 <jtan334@uwo.ca>
2024-03-20 19:39:46 +0800

    Points to the right profile, good for now?

--- frontend/Live/src/screens/LoginScreens/clubberLogin.js
+++ frontend/Live/src/screens/LoginScreens/clubberLogin.js
@@ -41,6 +41,12 @@ const LoginScreen = () => {
   
         // Navigate to the appropriate screen after login
         // navigation.navigate('HomeScreen', { screen: 'Search Venues' });      } else {
+          navigation.navigate('HomeScreen', {
+            screen: 'Profile', params: {
+                screen: 'ViewClubber'
+            }
+        });
+         
          navigation.navigate('HomeScreen');      
       } else {
 
